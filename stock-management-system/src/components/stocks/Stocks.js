8a3f1766654e97ba52ab0b5a9be48fb97f9b0d6f
Ajay <ajay_maddinani@dell.com>
2022-05-01 10:55:42 +0530

    Looped table

--- stock-management-system/src/components/stocks/Stocks.js
+++ stock-management-system/src/components/stocks/Stocks.js
@@ -2,6 +2,42 @@ import React from 'react'
 import { Icon, Modal, Menu, Table, Button } from 'semantic-ui-react'
 import AddEditStock from './AddEditStock'
 
+//Do an api call to populate this array
+var stocks = [
+{
+  portfolioName:'HCL Technologies Ltd',
+  stocks: '9',
+  createdOn: '02 Jan 2020',
+  modifiedOn: '13 Jan 2020'
+},
+{
+  portfolioName:'Reliance Petroliums',
+  stocks: '19',
+  createdOn: '12 Feb 2020',
+  modifiedOn: '30 Nov 2020'
+},
+{
+  portfolioName:'Facebook',
+  stocks: '93',
+  createdOn: '12 Mar 2020',
+  modifiedOn: '22 May 2020'
+},
+{
+  portfolioName:'Amazon',
+  stocks: '45',
+  createdOn: '14 Apr 2020',
+  modifiedOn: '16 Jul 2020'
+},
+{
+  portfolioName:'Starbucks',
+  stocks: '59',
+  createdOn: '02 Aug 2020',
+  modifiedOn: '23 Dec 2020'
+}
+
+];
+
+
 const Stocks = () => (
   <Table celled>
     <Table.Header>
@@ -14,26 +50,18 @@ const Stocks = () => (
     </Table.Header>
 
     <Table.Body>
-      <Table.Row>
-        <Table.Cell>
-                 HCL Technologies Ltd
-        </Table.Cell>
-        <Table.Cell>9</Table.Cell>
-        <Table.Cell>02 Jan 2020</Table.Cell>
-        <Table.Cell>13 Jan 2022</Table.Cell>
-      </Table.Row>
-      <Table.Row>
-        <Table.Cell>Reliance Petroliums</Table.Cell>
-        <Table.Cell>25</Table.Cell>
-        <Table.Cell>15 Mar 2021</Table.Cell>
-        <Table.Cell>31 Dec 2021</Table.Cell>
-      </Table.Row>
-      <Table.Row>
-        <Table.Cell>Bajaj Auto</Table.Cell>
-        <Table.Cell>67</Table.Cell>
-        <Table.Cell>22 Oct 2021</Table.Cell>
-        <Table.Cell>13 Apr 2022</Table.Cell>
-      </Table.Row>
+      {
+        stocks.map((stock) =>(
+          <Table.Row>
+          <Table.Cell>{stock.portfolioName}</Table.Cell>
+          <Table.Cell>{stock.stocks}</Table.Cell>
+          <Table.Cell>{stock.createdOn}</Table.Cell>
+          <Table.Cell>{stock.modifiedOn}</Table.Cell>
+        </Table.Row>
+        )
+        )
+      }
+     
     </Table.Body>
 
     <Table.Footer>
